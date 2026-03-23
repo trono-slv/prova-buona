1ba0f3f5521b29963cd643d9bb358be4413bdb73
trono-slv <trono.slv@gmail.com>
2026-03-23 16:01:13 +0100

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,26 @@
+// Quiz functionality implementation
+
+function showDisclaimer() {
+    const popup = document.getElementById('popup-disclaimer');
+    if (popup) {
+        popup.classList.remove('hidden');
+        
+        // Add event listeners only once
+        const btnAccept = document.getElementById('btn-accept');
+        const btnCancel = document.getElementById('btn-cancel');
+        
+        if (btnAccept) {
+            btnAccept.onclick = function() {
+                popup.classList.add('hidden');
+                startQuiz();
+            };
+        }
+        
+        if (btnCancel) {
+            btnCancel.onclick = function() {
+                popup.classList.add('hidden');
+                location.href = 'index.html';
+            };
+        }
+    }
+}
